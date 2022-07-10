c61265d8e9ffd8f3720ec8c6820ca11e93fd8d3d
Abhinava Sanyal <reltronx@gmail.com>
2022-07-11 01:32:37 +0530

    set jwt expiresIn to 1 day

--- auth/controllers/auth.controller.js
+++ auth/controllers/auth.controller.js
@@ -24,7 +24,7 @@ exports.generateTokens = user => {
           user,
           process.env.JWT_SECRET,
           {
-            expiresIn: '60s' // 60 min in seconds
+            expiresIn: '24h' 
           },
           (err, jwttoken) => {
             if (err) {
